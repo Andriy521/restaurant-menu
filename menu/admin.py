22a044ec3bd2d4f7dfd64a7f4fd5b88742047d94
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-03-26 12:58:42 +0100

    Add nem model ingredient with ManyToMany relation with dishes. Checked relations, all works corectly.

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from menu.models import DishType, Dish, Cook
+from menu.models import DishType, Dish, Cook, Ingredient
 
 admin.site.register(DishType)
 admin.site.register(Dish)
 admin.site.register(Cook)
+admin.site.register(Ingredient)
--- menu/models.py
+++ menu/models.py
@@ -20,12 +20,25 @@ class Cook(AbstractUser):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
 
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Dish(models.Model):
     name = models.CharField(
         max_length=100,
         unique=True,
     )
-    description = models.TextField()
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
     price = models.DecimalField(
         decimal_places=2,
         max_digits=10
@@ -34,6 +47,11 @@ class Dish(models.Model):
         DishType,
         on_delete=models.CASCADE,
         related_name="dish_type"
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        related_name="ingredients",
+        blank=True
     )
     cooks = models.ManyToManyField(
         Cook,
